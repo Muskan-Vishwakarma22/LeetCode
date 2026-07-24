8f7b756bfd96a801bbb14067a41c6ac7d60e9101
Muskan Vishwakarma <studymvish10@gmail.com>
2026-07-24 10:28:32 +0530

    Time: 2 ms (19.57%) | Memory: 61.4 MB (41.25%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -13,16 +13,23 @@ class Solution {
             left++;
             right--;
         }
-        for(int i=0; i<split;i++){
-            int temp = nums[i];
-            nums[i]=nums[split-1];
-            nums[split-1]=temp;
-            split--;
+        left = 0;
+        right = split-1;
+        while(left<right){
+            int temp = nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
         }
-        for(int i=0;i<max/2;i++){
-            int temp = nums[i];
-            nums[i]=nums[max-i-1];
-            nums[max-i-1] = temp;
+        left=0;
+        right=max-1;
+            while(left<right){
+            int temp = nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
         }
     }
 }
