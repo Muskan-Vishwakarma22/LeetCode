59df116a26163cb65ef20bd153a8342434bf7332
Muskan Vishwakarma <studymvish10@gmail.com>
2026-08-19 00:10:59 +0530

    Time: 1 ms (99.91%) | Memory: 46.8 MB (66.45%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
+                arr.add("FizzBuzz");
+            }
+            else if ((i + 1) % 3 == 0) {
+                arr.add("Fizz");
+            }
+            else if ((i + 1) % 5 == 0) {
+                arr.add("Buzz");
+            }
+            else {
+                arr.add(String.valueOf(i + 1));
+            }
+        }
+        return arr;
+    }
+}
